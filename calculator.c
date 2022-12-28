8f5e718a54faeacaf31db7a1b9c1db5b2c2ea4b7
heet2306 <heetmodh2306@gmail.com>
2022-12-28 12:51:33 +0530

    SE Assignments

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main ()
+{
+	int num1,num2,add,sub,div,mul,mod;
+	printf ("Enter the number 1 - ");
+	scanf("%d",&num1);
+	
+	printf ("Enter the number 2 - ");
+	scanf("%d",&num2);
+	
+	add=num1+num2;
+	sub=num1-num2;
+	div=num1/num2;
+	mul=num1*num2;
+	mod=num1%num2;
+	
+	printf("Addition Of Number %d",add);
+	printf("\nSubstration Of Number %d",sub);
+	printf("\nDivition Of Number %d",div);
+	printf("\nmultiplication Of Number %d",mul);
+	printf("\nModulo Of Number %d",mod);
+	
+	
+}
---
+++ printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+	printf("My Name Is Heet \n");
+	printf("My Birthdate Is 23/06/2001 \n");
+	printf("My Age is 22 \n");
+	printf("Im From Palanpur \n");
+	
+	return 0;
+	
+}
+
