5d7802042cf5eadf24e6a6646bad7fa053c656ee
heet2306 <heetmodh2306@gmail.com>
2022-12-29 17:29:29 +0530

    SE Assignment

--- Module3.1/areaofcircle.c
+++ Module3.1/areaofcircle.c
@@ -5,6 +5,6 @@ int main()
 	printf("Enter the radius =");
 	scanf("%f",&radius);
 	area=3.14*radius*radius;
-	printf("area of circle is %f",area);
+	printf("area of circle is = %f",area);
 	return 0;
 }
